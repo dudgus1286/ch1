a675f4a0c5bf5701d8a905be9e3ed0bceda52892
dudgus1286 <dudgus1286@gmail.com>
2024-05-08 12:42:36 +0900

    a

--- src/ch2/Account.java
+++ src/ch2/Account.java
@@ -2,4 +2,10 @@ package ch2;
 
 public class Account {
     private String owner;
+    private int balance;
+    private String accountNo;
+
+    public String getAccountNo() {
+        return accountNo;
+    }
 }
