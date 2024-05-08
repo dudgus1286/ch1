6b77a1240d0add9a34e214b378b18c8a1f5bacb2
dudgus1286 <dudgus1286@gmail.com>
2024-05-08 11:50:07 +0900

    인사말 추가2

--- src/ch1/Main.java
+++ src/ch1/Main.java
@@ -2,8 +2,8 @@ package ch1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("안녕하세요");
+        System.out.println("Main");
+        System.out.println("반갑습니다.");
     }
 
 }
